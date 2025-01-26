e80ed52e23405efbc4dd85db9021cb66a3eab719
Sergey Usenko <UsenkoS@terralink.ru>
2025-01-26 17:34:40 +0700

    3_service_layer_3_HWD2.patch

--- src/test/java/ru/javaops/cloudjava/aggregateservice/service/impl/AggregateServiceImplTest.java
+++ src/test/java/ru/javaops/cloudjava/aggregateservice/service/impl/AggregateServiceImplTest.java
@@ -7,14 +7,16 @@ import reactor.test.StepVerifier;
 import ru.javaops.cloudjava.aggregateservice.BaseTest;
 import ru.javaops.cloudjava.aggregateservice.dto.aggregate.MenuAggregate;
 import ru.javaops.cloudjava.aggregateservice.dto.aggregate.MenuAggregateList;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.RatedMenuItem;
 import ru.javaops.cloudjava.aggregateservice.dto.aggregate.RatedMenuSort;
 import ru.javaops.cloudjava.aggregateservice.dto.menu.Category;
 import ru.javaops.cloudjava.aggregateservice.dto.review.ReviewSort;
 import ru.javaops.cloudjava.aggregateservice.exception.MenuAggregateException;
 
+import java.util.Comparator;
+
 import static ru.javaops.cloudjava.aggregateservice.testutil.TestConstants.MENU_ONE_ID;
-import static ru.javaops.cloudjava.aggregateservice.testutil.TestDateProvider.expectedMenuAggregate;
-import static ru.javaops.cloudjava.aggregateservice.testutil.TestDateProvider.expectedMenuAggregateWithFallback;
+import static ru.javaops.cloudjava.aggregateservice.testutil.TestDateProvider.*;
 
 class AggregateServiceImplTest extends BaseTest {
 
@@ -51,6 +53,78 @@ class AggregateServiceImplTest extends BaseTest {
                 .expectNextMatches(response ->
                         response.equals(expectedMenuAggregateWithFallback()))
                 .verifyComplete();
+    }
+
+    @Test
+    void getMenuAggregateInfo_returnsErrorWhenMenuServiceTimedOut() {
+        stubForCorrectRatedReviewsList();
+        stubForMenuItemTimeout();
+
+        StepVerifier.create(getMenuAggregateMono())
+                .expectError(MenuAggregateException.class)
+                .verify();
+    }
+
+    @Test
+    void getMenuAggregateInfo_returnsFallbackWhenReviewServiceTimedOut() {
+        stubForRatedReviewsListTimeout();
+        stubForCorrectMenuItemResponse();
+
+        StepVerifier.create(getMenuAggregateMono())
+                .expectNextMatches(response ->
+                        response.equals(expectedMenuAggregateWithFallback()))
+                .verifyComplete();
+    }
+
+    @Test
+    void getMenusWithRatings_returnsCorrectResponse() {
+        stubForCorrectMenuListResponse();
+        stubForCorrectMenuRatingsResponse();
+
+        StepVerifier.create(getMenuRatingsMono())
+                .expectNextMatches(response ->
+                        response.equals(expectedMenuAggregateList(Comparator.comparing(RatedMenuItem::getCreatedAt).reversed())))
+                .verifyComplete();
+    }
+
+    @Test
+    void getMenusWithRatings_returnsErrorWhenMenuServiceUnavailable() {
+        stubForMenuItemList500Error();
+        stubForCorrectMenuRatingsResponse();
+
+        StepVerifier.create(getMenuRatingsMono())
+                .expectError(MenuAggregateException.class)
+                .verify();
+    }
+
+    @Test
+    void getMenusWithRatings_returnsErrorWhenReviewServiceUnavailable() {
+        stubForCorrectMenuListResponse();
+        stubForMenuRatings500Error();
+
+        StepVerifier.create(getMenuRatingsMono())
+                .expectError(MenuAggregateException.class)
+                .verify();
+    }
+
+    @Test
+    void getMenusWithRatings_returnsErrorWhenMenuServiceTimedOut() {
+        stubForMenuItemListTimeout();
+        stubForCorrectMenuRatingsResponse();
+
+        StepVerifier.create(getMenuRatingsMono())
+                .expectError(MenuAggregateException.class)
+                .verify();
+    }
+
+    @Test
+    void getMenusWithRatings_returnsErrorWhenReviewServiceTimedOut() {
+        stubForCorrectMenuListResponse();
+        stubForMenuRatingsTimeout();
+
+        StepVerifier.create(getMenuRatingsMono())
+                .expectError(MenuAggregateException.class)
+                .verify();
     }
 
     private Mono<MenuAggregate> getMenuAggregateMono() {
