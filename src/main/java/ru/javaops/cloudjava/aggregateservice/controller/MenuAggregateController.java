151949c1345cce78049adc33cd90ad725a72f80e
Sergey Usenko <UsenkoS@terralink.ru>
2025-01-26 17:38:47 +0700

    4_rest_layer_4_HWD3.patch

--- src/main/java/ru/javaops/cloudjava/aggregateservice/controller/MenuAggregateController.java
+++ src/main/java/ru/javaops/cloudjava/aggregateservice/controller/MenuAggregateController.java
@@ -1,10 +1,24 @@
 package ru.javaops.cloudjava.aggregateservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.MenuAggregate;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.MenuAggregateList;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.RatedMenuSort;
+import ru.javaops.cloudjava.aggregateservice.dto.menu.Category;
+import ru.javaops.cloudjava.aggregateservice.dto.review.ReviewSort;
 import ru.javaops.cloudjava.aggregateservice.service.AggregateService;
 
 @Tag(name = "MenuAggregateController", description = "REST API для работы с блюдами.")
@@ -15,4 +29,75 @@ import ru.javaops.cloudjava.aggregateservice.service.AggregateService;
 public class MenuAggregateController {
 
     private final AggregateService aggregateService;
+
+    @Operation(
+            summary = "${api.get-aggregate.summary}",
+            description = "${api.get-aggregate.description}"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response.getAggregateOk}"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "${api.response.badRequest}",
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "${api.response.notFound}",
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "${api.response.serviceUnavailable}",
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            )
+    })
+    @GetMapping("/{id}")
+    public Mono<MenuAggregate> getMenuAggregateInfo(@PathVariable("id")
+                                                    @Positive(message = "Идентификатор блюда не может быть <= 0.")
+                                                    Long menuId,
+                                                    @RequestParam(value = "sortBy", defaultValue = "date_asc")
+                                                    @NotBlank(message = "Параметр сортировки не может быть пустым.")
+                                                    String sortBy,
+                                                    @RequestParam(value = "from", defaultValue = "0")
+                                                    @PositiveOrZero(message = "Начало страницы пагинации не может быть < 0.")
+                                                    int from,
+                                                    @RequestParam(value = "size", defaultValue = "10")
+                                                    @Positive(message = "Размер страницы пагинации не может быть <= 0.")
+                                                    int size) {
+        log.info("Received request to GET info about menu item with ID={}. Sort: {}, from: {}, size: {}",
+                menuId, sortBy, from, size);
+        return aggregateService.getMenuAggregateInfo(menuId, ReviewSort.fromString(sortBy), from, size);
+    }
+
+    @Operation(
+            summary = "${api.get-aggregate-list.summary}",
+            description = "${api.get-aggregate-list.description}"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response.getAggregateListOk}"),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "${api.response.serviceUnavailable}",
+                    content = @Content(
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            )
+    })
+    @GetMapping
+    public Mono<MenuAggregateList> getMenusWithRatings(@RequestParam("category")
+                                                       @NotBlank(message = "Категория не может быть пустой.")
+                                                       String category,
+                                                       @RequestParam(value = "sortBy", defaultValue = "rate_desc")
+                                                       @NotBlank(message = "Параметр сортировки не может быть пустым.")
+                                                       String sortBy) {
+        log.info("Received request to GET info about menu items for category: {}", category);
+        return aggregateService.getMenusWithRatings(Category.fromString(category), RatedMenuSort.fromString(sortBy));
+    }
 }
--- src/test/java/ru/javaops/cloudjava/aggregateservice/controller/MenuAggregateControllerTest.java
+++ src/test/java/ru/javaops/cloudjava/aggregateservice/controller/MenuAggregateControllerTest.java
@@ -8,12 +8,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.javaops.cloudjava.aggregateservice.BaseTest;
 import ru.javaops.cloudjava.aggregateservice.dto.aggregate.MenuAggregate;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.MenuAggregateList;
+import ru.javaops.cloudjava.aggregateservice.dto.aggregate.RatedMenuItem;
+
+import java.util.Comparator;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static ru.javaops.cloudjava.aggregateservice.testutil.TestConstants.BASE_URL;
 import static ru.javaops.cloudjava.aggregateservice.testutil.TestConstants.MENU_ONE_ID;
-import static ru.javaops.cloudjava.aggregateservice.testutil.TestDateProvider.expectedMenuAggregate;
-import static ru.javaops.cloudjava.aggregateservice.testutil.TestDateProvider.expectedMenuAggregateWithFallback;
+import static ru.javaops.cloudjava.aggregateservice.testutil.TestDateProvider.*;
 
 @AutoConfigureWebTestClient(timeout = "20000")
 class MenuAggregateControllerTest extends BaseTest {
@@ -72,6 +75,64 @@ class MenuAggregateControllerTest extends BaseTest {
 
         webTestClient.get()
                 .uri(BASE_URL + "/" + MENU_ONE_ID)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.SERVICE_UNAVAILABLE.value());
+    }
+
+    @Test
+    void getMenusWithRatings_returnsCorrectResponse() {
+        stubForCorrectMenuRatingsResponse();
+        stubForCorrectMenuListResponse();
+
+        var expected = expectedMenuAggregateList(Comparator.comparing(RatedMenuItem::getCreatedAt).reversed());
+
+        webTestClient.get()
+                .uri(BASE_URL + "?category=DRINKS&sortBy=DATE_DESC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(MenuAggregateList.class)
+                .value(response -> {
+                    assertThat(response).isEqualTo(expected);
+                });
+    }
+
+    @Test
+    void getMenusWithRatings_returnsCorrectResponse_whenMenuServiceReturnsEmptyList() {
+        stubForEmptyMenuRatingsResponse();
+        stubForEmptyMenuListResponse();
+
+        webTestClient.get()
+                .uri(BASE_URL + "?category=DRINKS&sortBy=DATE_DESC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(MenuAggregateList.class)
+                .value(response -> {
+                    assertThat(response.getMenuItems()).isEmpty();
+                });
+    }
+
+    @Test
+    void getMenusWithRatings_returns503_whenMenuServiceUnavailable() {
+        stubForMenuItemList500Error();
+        stubForCorrectMenuRatingsResponse();
+
+        webTestClient.get()
+                .uri(BASE_URL + "?category=DRINKS&sortBy=DATE_DESC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.SERVICE_UNAVAILABLE.value());
+    }
+
+    @Test
+    void getMenusWithRatings_returns503_whenReviewServiceUnavailable() {
+        stubForMenuRatings500Error();
+        stubForCorrectMenuListResponse();
+
+        webTestClient.get()
+                .uri(BASE_URL + "?category=DRINKS&sortBy=DATE_DESC")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isEqualTo(HttpStatus.SERVICE_UNAVAILABLE.value());
